69822d4b3fdbb6cf570bf1d88631c7b62161f5c1
Didier Barvaux <didier@barvaux.org>
2014-02-22 15:22:06 +0100

    make test_timerfd_settime verbose by default to ease debug

--- tests/test_timerfd_settime.c
+++ tests/test_timerfd_settime.c
@@ -8,11 +8,25 @@
 #include <string.h>
 #include <unistd.h>
 
+static void print_test_status(const int test_ret, const int test_errno)
+{
+	if(test_ret == 0)
+	{
+		fprintf(stderr, "timerfd_settime() returned %d\n", test_ret);
+	}
+	else
+	{
+		fprintf(stderr, "timerfd_settime() returned %d and errno was set to %d\n",
+		        test_ret, test_errno);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	struct itimerspec old;
 	struct itimerspec new;
 	int fd;
+	int ret;
 
 	fd = timerfd_create(CLOCK_REALTIME, 0);
 	if(fd < 0)
@@ -23,28 +37,37 @@ int main(int argc, char *argv[])
 	}
 
 	/* bad fd */
-	assert(timerfd_settime(-1, 0, &new, NULL) != 0 &&
-	       (errno == EBADF || errno == EINVAL));
+	ret = timerfd_settime(-1, 0, &new, NULL);
+	print_test_status(ret, errno);
+	assert(ret != 0 && (errno == EBADF || errno == EINVAL));
 	/* bad pointer */
-	assert(timerfd_settime(fd, 0, NULL, NULL) != 0 && errno == EFAULT);
+	ret = timerfd_settime(fd, 0, NULL, NULL);
+	print_test_status(ret, errno);
+	assert(ret != 0 && errno == EFAULT);
 	/* ok without old value */
 	new.it_interval.tv_sec = 1;
 	new.it_interval.tv_nsec = 3;
 	new.it_value.tv_sec = 2;
 	new.it_value.tv_nsec = 4;
-	assert(timerfd_settime(fd, 0, &new, NULL) == 0);
+	ret = timerfd_settime(fd, 0, &new, NULL);
+	print_test_status(ret, errno);
+	assert(ret == 0);
 	/* ok with old value */
 	new.it_interval.tv_sec = 1;
 	new.it_interval.tv_nsec = 3;
 	new.it_value.tv_sec = 2;
 	new.it_value.tv_nsec = 4;
-	assert(timerfd_settime(fd, 0, &new, &old) == 0);
+	ret = timerfd_settime(fd, 0, &new, &old);
+	print_test_status(ret, errno);
+	assert(ret == 0);
 	/* ok, disarm */
 	new.it_interval.tv_sec = 0;
 	new.it_interval.tv_nsec = 0;
 	new.it_value.tv_sec = 0;
 	new.it_value.tv_nsec = 0;
-	assert(timerfd_settime(fd, 0, &new, &old) == 0);
+	ret = timerfd_settime(fd, 0, &new, &old);
+	print_test_status(ret, errno);
+	assert(ret == 0);
 
 	close(fd);
 	return 0;
